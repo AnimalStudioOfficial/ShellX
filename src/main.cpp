b838181f938370cc6caa254deed0addfa79602f1
Animal Studio <61629960+AnimalStudioOfficial@users.noreply.github.com>
2022-04-15 17:33:55 -0400

    Initial commit

---
+++ src/main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char *argv[])
+{
+	// cout << "Hello world!" << endl;
+
+	cout << " _______________________ " << endl;
+	cout << "|  ___________________  |" << endl;
+	cout << "| | Welcome To ShellX | |" << endl;
+	cout << "| |___________________| |" << endl;
+	cout << "|_______________________|" << endl;
+	cout << "                         " << endl;
+
+	cout << "What Do you whant to do:" << endl;
+	static string UserInput;
+
+}
